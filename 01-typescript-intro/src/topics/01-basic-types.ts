30f617a7d6b0cae55a2f03b1c0ca76f7535d65a5
Nestor Bardel <nestorbardel@gmail.com>
2024-07-04 09:58:54 -0300

    first commit

---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,13 @@
+const name: string = 'Facha';
+let hpPoint: number | "FULL";
+const isAlive: boolean = true;
+
+hpPoint = 'FULL';
+
+console.log({
+    name, hpPoint, isAlive
+});
+
+
+
+export {};
