df7d8bfe7897d56a3428f8e4448d39a6fb14a70c
Nestor <nestorbardel@gmail.com>
2024-07-27 18:07:12 -0300

    terminada seccion 4

---
+++ 02-bases/src/app/counter/components/counter.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { CounterComponent } from "./counter/counter.component";
+import { CommonModule } from "@angular/common";
+
+@NgModule({
+  declarations: [
+    CounterComponent
+  ],
+  exports: [
+    CounterComponent
+  ],
+  imports: [CommonModule],
+})
+export class CounterModule { }
