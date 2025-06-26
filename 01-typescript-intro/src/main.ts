aae474ea0e8fea033e3663c9906a2dc1bd92251a
Nestor <nestorbardel@gmail.com>
2025-06-25 22:37:00 -0300

    21 - Terminado

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -3,8 +3,9 @@ import './style.css'
 // import './topics/02-object-interface.ts';
 // import './topics/03-functions.ts';
 // import './topics/04-homework-types.ts';
-import './topics/05-basic-destructuring.ts';
-
+// import './topics/05-basic-destructuring.ts';
+// import './topics/06-function-destructuring.ts';
+import './topics/07-import-export.ts';
 const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `Hola Mundo!`;
