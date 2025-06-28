06ac55a04be9a725720448778572a82e04d10732
Nestor <nestorbardel@gmail.com>
2025-06-28 12:27:48 -0300

    38 - Terminado

--- 02-bases/src/app/pages/counter/counter-page.component.ts
+++ 02-bases/src/app/pages/counter/counter-page.component.ts
@@ -1,15 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 
 @Component({
-  template: `
-    <h1>Counter {{counter}}</h1>
-    <button (click)="increaseBy(1)">+1</button>
-  `
+  templateUrl: './counter-page.component.html',
+  styleUrl:'./counter-page.component.css'
 })
 export class CounterPageComponent {
-  counter = 15;
+  counter = 10;
+  counterSignal = signal(10);
 
   increaseBy(value: number):void{
     this.counter += value;
+    // this.counterSignal.set(this.counterSignal() + value);
+    this.counterSignal.update((current) => current + value)
+  }
+
+  resetCounter():void{
+    this.counter = 0;
+    this.counterSignal.set(0);
   }
 }
