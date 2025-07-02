51db829700dd253ba60a1ece9dcd08569571f38f
Nestor <nestorbardel@gmail.com>
2025-07-02 00:49:01 -0300

    51 - Terminado

--- 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+interface Character{
+  id: number;
+  name: string;
+  power: number;
+}
 
 @Component({
-  selector: 'app-dragonball-page.component',
   imports: [],
   templateUrl: './dragonball-page.component.html',
-  styleUrl: './dragonball-page.component.css'
 })
 export class DragonballPageComponent {
-
+  characters = signal<Character[]>([
+    {
+      id: 1,
+      name: 'Goku',
+      power: 9001,
+    },
+    {
+      id:2,
+      name: 'Vegeta',
+      power: 8000,
+    },
+    {
+      id:3,
+      name:'Piccolo',
+      power:3000,
+    }
+  ]);
 }
