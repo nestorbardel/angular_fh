2ee64bde30a11a62459b963993f20a8f13bda5da
Nestor <nestorbardel@gmail.com>
2025-07-18 14:40:44 -0300

    Eliminar comentarios y mejorar la lógica de adición de personajes en la página DragonBall.

--- 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -43,7 +43,24 @@ export class DragonballPageComponent {
   })
 
   addCharacter(){
-    console.log("Nombre: " , this.name());
-    console.log("Poder: ", this.power());
+    // console.log("Nombre: " , this.name());
+    // console.log("Poder: ", this.power());
+    if(!this.name() || !this.power() || this.power() <= 0){
+      return;
+    }
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    }
+
+    this.characters.update((list) => [...list, newCharacter]);
+    this.resetFields();
+  }
+
+  resetFields(){
+    this.name.set('');
+    this.power.set(0);
   }
 }
