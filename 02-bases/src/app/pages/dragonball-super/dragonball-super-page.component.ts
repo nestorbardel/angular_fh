4e71a37e60c0c17185716e8578151938c9779d61
Nestor Bardel <nestorbardel@gmail.com>
2025-07-19 00:37:36 -0300

    feat(dragonball): implement character addition and listing functionality

--- 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,39 +1,16 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component,  inject } from '@angular/core';
+import { CharacterListComponent } from '../../components/dragonball/character-list/character-list.component';
+import { CharacterAddComponent } from "../../components/dragonball/character-add/character-add.component";
+import { DragonballService } from '../../services/dragonball.service';
 
-interface Character{
-  id: number;
-  name: string;
-  power: number;
-}
+
 
 @Component({
   templateUrl: './dragonball-super-page.component.html',
-  selector:'dragonball-super'
+  selector: 'dragonball-super',
+  imports: [CharacterListComponent, CharacterAddComponent],
 })
 export class DragonballSuperPageComponent {
-  name = signal('');
-  power = signal(0);
-  characters = signal<Character[]>([
-    {
-      id: 1,
-      name: 'Goku',
-      power: 9001,
-    },
-    {
-      id:2,
-      name: 'Vegeta',
-      power: 8000,
-    },
-  ]);
+  public dragonballService = inject(DragonballService);
 
-  powerClasses = computed(() => {
-    return {
-      'text-danger': true,
-    }
-  })
-
-  addCharacter(){
-    console.log("Nombre: " , this.name());
-    console.log("Poder: ", this.power());
-  }
 }
---
+++ 02-bases/src/app/services/dragonball.service.ts
@@ -0,0 +1,29 @@
+import { effect, Injectable, signal } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+
+@Injectable({providedIn: 'root'})
+export class DragonballService {
+  characters = signal<Character[]>([
+    {
+      id: 1,
+      name: 'Goku',
+      power: 9001,
+    },
+    {
+      id: 2,
+      name: 'Vegeta',
+      power: 8000,
+    },
+  ]);
+
+  saveToLocalStorage = effect(() => {
+    console.log(`Character count: ${this.characters().length}`);
+    localStorage.setItem('characters', JSON.stringify(this.characters()));
+
+  });
+
+  addCharacter(character: Character) {
+    this.characters.update((list) => [...list, character]);
+  }
+
+}
