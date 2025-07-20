6450744e6e31a0295e001cfac36debb83ac89991
Nestor Bardel <nestorbardel@gmail.com>
2025-07-20 08:59:44 -0300

    feat(side-menu): implement dynamic side menu options with routing and icons

--- 03-gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
+++ 03-gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption {
+  icon: string;
+  label: string;
+  route: string;
+  subLabel: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
-  templateUrl: './side-menu-options.html'
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './side-menu-options.html',
 })
 export class SideMenuOptions {
-
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar Gifs',
+      route: '/dashboard/search',
+    },
+  ];
 }
